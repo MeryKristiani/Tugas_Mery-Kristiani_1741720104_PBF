6baaa13e609dc602c24c8ee1ea559d5fc9479ad2
Mery Kristiani <merykristiani2@gmail.com>
2020-02-19 19:52:32 +0700

    Menambahkan Footer

---
+++ pertemuan-3/merykristiani-minggu3/src/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function Footer() {
+  return (
+    <nav class="navbar navbar-expand-lg">
+      <a class="navbar-brand">merykristiani2@gmail.com</a>
+    </nav>
+  );
+}
+
+export default Footer;
--- pertemuan-3/merykristiani-minggu3/src/index.js
+++ pertemuan-3/merykristiani-minggu3/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Footer from './footer';
 import * as serviceWorker from './serviceWorker';
 
 const NavBar = () => {
@@ -38,6 +39,7 @@ const NavBar = () => {
 
 ReactDOM.render(<NavBar />, document.getElementById('navbar'));
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Footer />, document.getElementById('footer'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
