ef6c433f0005be258abf9d8799d86a9ee90e35e7
Mery Kristiani <merykristiani2@gmail.com>
2020-02-19 19:49:57 +0700

    Halamn Biodata

--- pertemuan-3/merykristiani-minggu3/src/App.js
+++ pertemuan-3/merykristiani-minggu3/src/App.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './aing.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p className="Title">BIODATA</p>
+        <div class="row">
+          <div class="col-5">
+          <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div class="col-1">
+            <p>Nama:</p>
+            <p>TTL:</p>
+            <p>Gender:</p>
+            <p>Alamat:</p>
+            <p>Status:</p>
+            <p>Hobi:</p>
+            <p>Email:</p>
+            <p>Github:</p>
+          </div>
+          <div class="col-6">
+            <p>Mery Kristiani</p>
+            <p>Blitar, 2 Maret 1998</p>
+            <p>Perempuan</p>
+            <p>D'wiga Regency A5/11</p>
+            <p>Mahasiswa</p>
+            <p>Badminton</p>
+            <p>merykristiani2@gmail.com</p>
+            <p>https://github.com/MeryKristiani/</p>
+          </div>
+      </div>
       </header>
     </div>
   );
