6af3997a8fba23b44d7699710f0f40240b613343
MeryKristiani <merykristiani2@gmail.com>
2020-03-05 23:28:59 +0700

    Tugas-Minggu5

--- pertemuan-5/src/Login/BlogPost/BlogPost.jsx
+++ pertemuan-5/src/Login/BlogPost/BlogPost.jsx
@@ -3,18 +3,21 @@ import './BlogPost.css';
 import Post from "../../component/BlogSpot/Post";
 
 class BlogPost extends Component {      // komponen state dari React untuk statefull component
-    state =  {                          // variabel array yang digunakan untuk menyimpan data API
-        listArtikel:[],
+    state = {
+        listArtikel: [],
         insertArtikel: {
-            userId: 1,
-            id: 1,
-            title: "",
-            body: ""
+          id: 1,
+          nim: 1,
+          nama: "",
+          alamat: "",
+          hp: "",
+          angkatan: 1,
+          status: ""
         }
-    }
+      };
 
     ambilDataDariServerAPI = () => {
-        fetch('http://localhost:3001/posts?_sort=id&_order=desc')
+        fetch('http://localhost:3001/posts')
         .then(response => response.json())
         .then(jsonHasilAmbilDariAPI => {
             this.setState( {
@@ -60,31 +63,112 @@ class BlogPost extends Component {      // komponen state dari React untuk state
 
     render() {
         return (
-            <div className="post-artikel">
-                <div className="form pb-2 border-bottom">
-                    <div className="form-group row">
-                        <label htmlFor="title" className="col-sm-2 col-form-label">Judul</label>
-                        <div className="col-sm-10">
-                            <input type="text" className="form-control" id="title" name="title" onChange={this.handleTambahArtikel}/>
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                    <label htmlFor="body" className="col-sm-2 col-form-label">Isi</label>
-                    <div className="col-sm-10">
-                        <textarea className="form-control" id="body" name="body" rows="3" onChange={this.handleTambahArtikel}></textarea>
-                    </div>
-                </div>
-                <button type="submit" className="btn btn-primary" onClick={this.handleTombolSimpan}>Simpan</button>
-                </div>
-                <h2>Daftar Artikel</h2>
-                {   
-                    this.state.listArtikel.map(artikel => {     // looping dan masukkan untuk setiap data yang ada di listArtikel ke variabel artikel
-                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} idArtikel={artikel.id} hapusArtikel={this.handleHapusArtikel}/>      // mappingkan data json dari API sesuai dengan kategorinya
-                    })
-                }
+          <div className="post-artikel">
+            <div className="form pb-2 border-bottom">
+              <div className="form-group row"></div>
+              <label htmlFor="title" className="col-form-label">
+                NIM
+              </label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  className="form-nim"
+                  id="nim"
+                  name="nim"
+                  onChange={this.handleTambahArtikel}
+                />
+              </div>
+              <div className="form-group row"></div>
+              <label htmlFor="title" className="col-form-label">
+                Nama
+              </label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  className="form-nama"
+                  id="nama"
+                  name="nama"
+                  onChange={this.handleTambahArtikel}
+                />
+              </div>
+              <div className="form-group row"></div>
+              <label htmlFor="title" className="col-form-label">
+                Alamat
+              </label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  className="form-alamat"
+                  id="alamat"
+                  name="alamat"
+                  onChange={this.handleTambahArtikel}
+                />
+              </div>
+              <div className="form-group row"></div>
+              <label htmlFor="title" className="col-form-label">
+                Nomor HP:
+              </label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  className="form-hp"
+                  id="hp"
+                  name="hp"
+                  onChange={this.handleTambahArtikel}
+                />
+              </div>
+              <div className="form-group row"></div>
+              <label htmlFor="title" className="col-form-label">
+                Angkatan
+              </label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  className="form-angkatan"
+                  id="angkatan"
+                  name="angkatan"
+                  onChange={this.handleTambahArtikel}
+                />
+              </div>
+              <div className="form-group row"></div>
+              <label htmlFor="title" className="col-form-label">
+                Status
+              </label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  className="form-status"
+                  id="status"
+                  name="status"
+                  onChange={this.handleTambahArtikel}
+                />
+              </div>
             </div>
-        )
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={this.handleTombolSimpan}
+            >
+              Simpan
+            </button>
+            <h2>Daftar Mahasiswa</h2>
+            {this.state.listArtikel.map(artikel => {
+              return (
+                <Post
+                  idMahasiswa={artikel.id}
+                  nim={artikel.nim}
+                  nama={artikel.nama}
+                  alamat={artikel.alamat}
+                  hp={artikel.hp}
+                  angkatan={artikel.angkatan}
+                  status={artikel.status}
+                  hapusArtikel={this.handleHapusArtikel}
+                />
+              );
+            })}
+          </div>
+        );
+      }
     }
-}
 
 export default BlogPost;
