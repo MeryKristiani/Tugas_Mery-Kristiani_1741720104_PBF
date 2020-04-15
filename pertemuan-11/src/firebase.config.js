ed003b56e326a05c2e3256c5e223eab71a3d7047
MeryKristiani <merykristiani2@gmail.com>
2020-04-15 11:48:48 +0700

    Pertemuan-11

---
+++ pertemuan-11/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBQjL23ebUL4hdZrCZU-UXrnLiqjABfiu8",
+    authDomain: "pbf-pertemuan11.firebaseapp.com",
+    databaseURL: "https://pbf-pertemuan11.firebaseio.com",
+    projectId: "pbf-pertemuan11",
+    storageBucket: "pbf-pertemuan11.appspot.com",
+    messagingSenderId: "857322342100",
+    appId: "1:857322342100:web:0757552e36639dff289d41",
+    measurementId: "G-C3KTGDQCEH"
+  };
