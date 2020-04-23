8a63e7f5a7024ff3e55591338b25550a4b74a6d6
MeryKristiani <merykristiani2@gmail.com>
2020-04-23 10:57:51 +0700

    pertemuan-12

---
+++ pertemuan-12/src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD27MsTJzG7EzC49nVBrF7ETqY9boMTgF8",
+    authDomain: "pbf-pertemuan12.firebaseapp.com",
+    databaseURL: "https://pbf-pertemuan12.firebaseio.com",
+    projectId: "pbf-pertemuan12",
+    storageBucket: "pbf-pertemuan12.appspot.com",
+    messagingSenderId: "660481773959",
+    appId: "1:660481773959:web:c4cabbbf6a0a6782304322",
+    measurementId: "G-0G37X1G2FB"
+};
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;
