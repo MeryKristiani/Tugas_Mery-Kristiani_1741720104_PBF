e6c1af647816c037defb6289b46cc8c39a03c344
MeryKristiani <merykristiani2@gmail.com>
2020-04-30 22:40:26 +0700

    Pertemuan-13

---
+++ pertemuan-13/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD27MsTJzG7EzC49nVBrF7ETqY9boMTgF8",
+    authDomain: "pbf-pertemuan12.firebaseapp.com",
+    databaseURL: "https://pbf-pertemuan12.firebaseio.com",
+    projectId: "pbf-pertemuan12",
+    storageBucket: "pbf-pertemuan12.appspot.com",
+    messagingSenderId: "660481773959",
+    appId: "1:660481773959:web:c4cabbbf6a0a6782304322",
+    measurementId: "G-0G37X1G2FB"
+  };
+
+  export default firebaseConfig;
